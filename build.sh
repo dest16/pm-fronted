6af221d607356f25df0ff73270c2c1a841e925d7
dest16 <dest16@163.com>
2023-05-18 10:57:33 +0800

    fix sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-npm --registry https://registry.npm.taobao.org install
-npm run build
+npm --registry https://registry.npm.taobao.org install --loglevel verbose
+npm run build --loglevel verbose
 rm -rf /srv/fronted
 cp -r /srv/temp/dist /srv/fronted
 rm -rf /srv/temp
